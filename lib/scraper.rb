c4b1c03297ba6c73671e05f419b41b9a0d8bcf06
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-05-08 22:37:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,65 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = open(index_url)
+    roster = Nokogiri::HTML(html)
     
+    students = []
+    
+    roster.css("div.student-card").each do |student|
+      hash = {
+        :name => student.css("h4.student-name").text,
+        :location => student.css("p.student-location").text,
+        :profile_url => student.css("a").attribute("href").value
+      }
+      students << hash
+    end
+    students
   end
 
+
+
   def self.scrape_profile_page(profile_url)
+    html = open(profile_url)
+    student_page = Nokogiri::HTML(html)
     
+    hash = {
+      :profile_quote => student_page.css("div.profile-quote").text,
+      :bio => student_page.css("div.description-holder p").text
+    }
+    
+    student_page.css("div.social-icon-container a").each do |social|
+      link = social.css("a").attribute("href").value
+      binding.pry
+      if link.include?("twitter")
+        hash[:twitter] = link 
+        elsif link.include?("linkedin")
+        hash[:linkedin] = link 
+        elsif link.include?("github")
+        hash[:github] = link
+        elsif link.include?("flatiron")
+        hash[:blog] = link 
+      end
+    end
+    hash
   end
+
+
 
 end
 
+# social: student_page.css("div.social-icon-container")
+# social.css("a").attribute("href").value
+  # :twitter=> 
+  # :linkedin=> 
+  # :github=> 
+  # :blog=>
+  # :profile_quote=> student_page.css("div.profile-quote").text
+  # :bio=> student_page.css("div.description-holder p").text
+  
+  
+  
